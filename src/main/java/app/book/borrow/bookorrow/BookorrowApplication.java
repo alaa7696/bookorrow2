32a76486b24ada1b47f7af4f32996d41d15f69ca
aearsalane <aearsalane@sqli.com>
2019-04-24 16:27:42 +0100

    first commit init spring boot project

---
+++ src/main/java/app/book/borrow/bookorrow/BookorrowApplication.java
@@ -0,0 +1,13 @@
+package app.book.borrow.bookorrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookorrowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookorrowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/book/borrow/bookorrow/BookorrowApplicationTests.java
@@ -0,0 +1,16 @@
+package app.book.borrow.bookorrow;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookorrowApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
